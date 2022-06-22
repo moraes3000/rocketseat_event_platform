939e11bc269a3480600cecdd4d106ef805c812fd
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-06-22 12:05:02 -0300

    component Lesson

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,26 @@
+import { CheckCircle } from 'phosphor-react'
+export function Lesson() {
+  return (
+    <a href='#'>
+      <span className='text-gray-300'>
+        Terça - 22  de junho 19h00
+      </span>
+
+      <div className='rounded border border-gray-500 p-4  mt-2'>
+        <header className='flex items-center justify-between'>
+          <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
+            <CheckCircle size={20} />  Conteúdo liberado
+          </span>
+          <span className='text-xs   px-2 py-[0.125rem] text-white border border-green-300 font-bold'>
+            AO VIVO
+          </span>
+        </header>
+      </div>
+
+      <strong className='text-gray-200 mt-5 block'>
+        Abertura do evento Ignite Lab
+      </strong>
+
+    </a>
+  )
+}
