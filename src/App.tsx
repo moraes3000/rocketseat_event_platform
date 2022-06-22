ec97de64dbb0f72262d616ff9ccbc97496c80bd9
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-06-21 21:19:55 -0300

    Component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query{
-      lessons {
-      stage
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string
-}
+import { Event } from "./pages/Event"
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
   return (
     <>
-      <h1 className="text-2x">Hello World</h1>
-      <ul>
-        {data?.lessons.map(lesson => {
-          return <li>{lesson.title}</li>
-        })}
-      </ul>
+      <Event />
     </>
   )
 }
