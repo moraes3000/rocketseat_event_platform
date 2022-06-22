e19448dc81188cf685dce624da263b4b8d85af08
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-06-22 12:36:37 -0300

    query component lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,25 +1,47 @@
-import { CheckCircle } from 'phosphor-react'
-export function Lesson() {
+import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class'
+}
+
+export function Lesson({ title, slug, availableAt, type }: LessonProps) {
+  const isLessonAvailable = isPast(availableAt);
+  const availableDataFormat = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'K'h'mm", {
+    locale: ptBR
+  })
+
   return (
     <a href='#'>
       <span className='text-gray-300'>
-        Terça - 22  de junho 19h00
+        {availableDataFormat}
       </span>
 
       <div className='rounded border border-gray-500 p-4  mt-2'>
         <header className='flex items-center justify-between'>
-          <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
-            <CheckCircle size={20} />  Conteúdo liberado
-          </span>
+
+          {isLessonAvailable ?
+            (<span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
+              <CheckCircle size={20} />  Conteúdo liberado
+            </span>)
+            : (
+              <span className='text-sm text-orange-500 font-medium flex items-center gap-2'>
+                <Lock size={20} />  Em  Breve
+              </span>
+            )}
+
           <span className='text-xs   px-2 py-[0.125rem] text-white border border-green-300 font-bold'>
-            AO VIVO
+            {type == 'live' ? "AO VIVO" : "AULA PRÁTICA"}
           </span>
         </header>
+        <strong className='text-gray-200 mt-5 block'>
+          {title}
+        </strong>
       </div>
-
-      <strong className='text-gray-200 mt-5 block'>
-        Abertura do evento Ignite Lab
-      </strong>
 
     </a>
   )
