fb592cfd1abc9ca0beb7e4a9571578a33da6eb74
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-06-24 12:03:30 -0300

    add lib VimeJS

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,17 @@
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { DefaultUi, Player, Youtube } from "@vime/react";
+import '@vime/core/themes/default.css'
 
 export function Video() {
   return (
     <div className='flex-1'>
       <div className='bg-black flex justify-center'>
-        <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video '></div>
+        <div className='h-full w-full max-w-[1100px] max-h-[50vh] aspect-video '>
+          <Player>
+            <Youtube videoId="SO4-izct7Mc" />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className='p-8  w-full max-w-[1100px] mx-auto'>
